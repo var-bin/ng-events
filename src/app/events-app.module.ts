e4df6868da9aa9d1492d0a7cee79eb37f324669a
var-bin <var-bin@users.noreply.github.com>
2018-07-26 16:07:03 +0300

    Added `EventsListComponent` to declarations

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from "./events/events-list.component";
 
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
