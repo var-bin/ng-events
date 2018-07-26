6611d43d7b65cc7d9abea228366f84518371f774
var-bin <var-bin@users.noreply.github.com>
2018-07-26 16:38:36 +0300

    Edit `events-list` component HTML

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -3,9 +3,21 @@ import { Component } from "@angular/core";
 @Component({
   selector: "events-list",
   template: `
-    <div>
+    <section>
       <h1>Upcomming Angular Events</h1>
-    </div>
+      <hr/>
+      <div class="well hoverwell thumbnail">
+        <h2>{{event.name}}</h2>
+        <div>Date: {{event.date}}</div>
+        <div>Time: {{event.time}}</div>
+        <div>Price: \${{event.price}}</div>
+        <div>
+          <span>Location: {{event.location.address}}</span>
+          <span>&nbsp;</span>
+          <span>{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+      </div>
+    </section>
   `
 })
 
