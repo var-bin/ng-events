a5809d94bef3662db2ef43ece07b57692beb21a7
var-bin <var-bin@users.noreply.github.com>
2018-07-26 21:51:24 +0300

    Replace single quotes -> double quotes

--- src/app/events-app.component.spec.ts
+++ src/app/events-app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { EventsAppComponent } from './events-app.component';
-describe('AppComponent', () => {
+import { TestBed, async } from "@angular/core/testing";
+import { EventsAppComponent } from "./events-app.component";
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,15 +8,15 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it("should create the app", async(() => {
     const fixture = TestBed.createComponent(EventsAppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it('should render title in a h1 tag', async(() => {
+  it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(EventsAppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Events App');
+    expect(compiled.querySelector("h1").textContent).toContain("Events App");
   }));
 });
