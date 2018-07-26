9b04058877598a23da71601ffa44171393e20e1b
var-bin <var-bin@users.noreply.github.com>
2018-07-26 22:05:16 +0300

    Fixed e2e tests errors

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe("ng-events App", () => {
 
   it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual("Welcome to app!");
+    expect(page.getParagraphText()).toEqual("Upcomming Angular Events");
   });
 });
