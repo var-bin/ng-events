852ec60a5a0d80d4eb42a79fe2ed24ce2249f4c5
var-bin <var-bin@users.noreply.github.com>
2018-07-26 21:51:49 +0300

    Replace single quotes -> double quotes

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumnail.component';
-import { NavComponent } from './nav/nav.component';
+import { EventsAppComponent } from "./events-app.component";
+import { EventsListComponent } from "./events/events-list.component";
+import { EventThumbnailComponent } from "./events/event-thumnail.component";
+import { NavComponent } from "./nav/nav.component";
 
 
 @NgModule({
