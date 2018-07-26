cef58e188ac4dc00504d6f9b95eaa12c312e94ae
var-bin <var-bin@users.noreply.github.com>
2018-07-26 21:32:25 +0300

    Added `nav` component to declarations block

--- src/app/events-app.module.ts
+++ src/app/events-app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumnail.component";
+import { NavComponent } from "./nav/nav.component";
 
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
