058b23671927b216558099fa0d3413a6d9c5fb7d
var-bin <var-bin@users.noreply.github.com>
2018-07-26 17:57:02 +0300

    Replace HTML part to separated file

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,23 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "events-list",
-  template: `
-    <section>
-      <h1>Upcomming Angular Events</h1>
-      <hr/>
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
-      </div>
-    </section>
-  `
+  templateUrl: "./events-list.component.html"
 })
 
 export class EventsListComponent {
