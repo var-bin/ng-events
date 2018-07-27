b87a78d93a8f9363d810f428e50232298661c921
var-bin <var-bin@users.noreply.github.com>
2018-07-27 21:07:40 +0300

    Added styles for `event-thumbnail` component

--- src/app/events/event-thumnail.component.ts
+++ src/app/events/event-thumnail.component.ts
@@ -2,7 +2,12 @@ import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "event-thumbnail",
-  templateUrl: "./event-thumbnail.component.html"
+  templateUrl: "./event-thumbnail.component.html",
+  styles: [`
+    .thumbnail { min-height: 248px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 
 export class EventThumbnailComponent {
