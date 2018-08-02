1d0ae3e153eaadf8db2cab9d991ebb587f7103ca
var-bin <var-bin@users.noreply.github.com>
2018-08-02 22:37:45 +0300

    Replace `<events-list></events-list>` -> `<router-outlet></router-outlet>` for allowing routes feature

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "events-app",
   template: `
     <events-nav></events-nav>
-    <events-list></events-list>
+    <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {}
