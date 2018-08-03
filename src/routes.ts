48f916431e2ffb300235985a3bd859b343e44e40
var-bin <var-bin@users.noreply.github.com>
2018-08-03 22:58:24 +0300

    Added a new route for `create-event` component

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,13 @@ import { Routes } from "@angular/router";
 
 import { EventsListComponent } from "./app/events/events-list.component";
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
+import { CreateEventComponent } from "./app/events/create-event/create-event.component";
 
 const appRoutes: Routes = [
+  {
+    path: "events/new",
+    component: CreateEventComponent
+  },
   {
     path: "events",
     component: EventsListComponent
