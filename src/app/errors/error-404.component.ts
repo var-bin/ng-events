627002f4fb6e8df3668d455e8e25705461200bc2
var-bin <var-bin@users.noreply.github.com>
2018-08-06 17:10:27 +0300

    Added `error-404` component for showing 404 error

---
+++ src/app/errors/error-404.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: `
+    <h1 class="error-message">404</h1>
+  `,
+  styles: [`
+    .error-message {
+      margin-top: 150px;
+      font-size: 170px;
+      text-align: center;
+    }
+  `]
+})
+
+export class Error404Component {}
