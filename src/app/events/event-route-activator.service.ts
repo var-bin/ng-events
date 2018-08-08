3532d27989f2f904823fe87a92ae400694d8f513
var-bin <var-bin@users.noreply.github.com>
2018-08-08 21:27:07 +0300

    Reformatted. Added `toastrService` for showing warning message in case a user doesn't save an event

--- src/app/events/event-route-activator.service.ts
+++ src/app/events/event-route-activator.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from "@angular/core";
 import { Router, CanActivate, ActivatedRouteSnapshot, CanDeactivate } from "@angular/router";
-import { EventService } from "./shared/event.service";
 
 import { CreateEventComponent } from "./create-event/create-event.component";
 
+import { EventService } from "./shared/event.service";
+import { ToastrService } from "../common/toastr.service";
+
 @Injectable()
 export class EventRouteActivator implements CanActivate, CanDeactivate<any> {
-  constructor(private router: Router, private eventService: EventService) {}
+  constructor(
+    private router: Router,
+    private eventService: EventService,
+    private toastrService: ToastrService
+  ) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
     const isEventExisted = !!this.eventService.getEvent(+route.params["id"]);
@@ -20,6 +26,8 @@ export class EventRouteActivator implements CanActivate, CanDeactivate<any> {
 
   canDeactivate(component: CreateEventComponent) {
     if (component.isDirty) {
+      this.toastrService.warning("You have not saved this event!");
+
       return false;
     }
 
