af3e469104e6c6e62cfad7196498a19ea6c67c39
var-bin <var-bin@users.noreply.github.com>
2018-08-08 17:14:07 +0300

    Using `EventRouteActivator` service within `canActivate` guard

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,9 @@ import { Routes } from "@angular/router";
 import { EventsListComponent } from "./app/events/events-list.component";
 import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
 import { CreateEventComponent } from "./app/events/create-event/create-event.component";
+import { Error404Component } from "./app/errors/error-404.component";
+
+import { EventRouteActivator } from "./app/events/event-route-activator.service";
 
 const appRoutes: Routes = [
   {
@@ -15,7 +18,12 @@ const appRoutes: Routes = [
   },
   {
     path: "events/:id",
-    component: EventDetailsComponent
+    component: EventDetailsComponent,
+    canActivate: [EventRouteActivator]
+  },
+  {
+    path: "404",
+    component: Error404Component
   },
   {
     path: "",
