8245fdf0374a9144af5cb7d673bd746352b6a362
var-bin <var-bin@users.noreply.github.com>
2018-08-08 17:13:00 +0300

    Added `event-route-activator` service for guarding routes

---
+++ src/app/events/event-route-activator.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { Router, CanActivate, ActivatedRouteSnapshot } from "@angular/router";
+import { EventService } from "./shared/event.service";
+
+@Injectable()
+export class EventRouteActivator implements CanActivate {
+  constructor(
+    private router: Router,
+    private eventService: EventService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot) {
+    const isEventExisted = !!this.eventService.getEvent(+route.params["id"]);
+
+    if (!isEventExisted) {
+      this.router.navigate(["/404"]);
+    }
+
+    return isEventExisted;
+  }
+}
