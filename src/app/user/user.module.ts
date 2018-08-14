62be767155ec0441193a36decc4bfa058db860b5
var-bin <var-bin@users.noreply.github.com>
2018-08-14 16:19:47 +0300

    Added `LoginComponent` to `declarations`. Import `FormsModule` to `imports`

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { UserProfileComponent } from "./user-profile.component";
+import { LoginComponent } from "./login/login.component";
 
 import { userRoutes } from "./user.routes";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
-    UserProfileComponent
+    UserProfileComponent,
+    LoginComponent
   ],
   providers: []
 })
