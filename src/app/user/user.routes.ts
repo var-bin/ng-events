2987b5616bb0f0d9e9ca29e6a4f573790d9c6dc7
var-bin <var-bin@users.noreply.github.com>
2018-08-14 16:20:09 +0300

    Added a new path for `LoginComponent`

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from "@angular/router";
 
 import { UserProfileComponent } from "./user-profile.component";
+import { LoginComponent } from "./login/login.component";
 
 const userRoutes: Routes = [
   {
     path: "profile",
     component: UserProfileComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
   }
 ];
 
