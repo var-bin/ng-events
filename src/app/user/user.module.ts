f5c6b197ced30bfe9fe79d9d86ee12e5c4239fd5
var-bin <var-bin@users.noreply.github.com>
2018-08-23 23:15:55 +0300

    Added `ReactiveFormsModule` to `imports`

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { UserProfileComponent } from "./profile/profile.component";
 import { LoginComponent } from "./login/login.component";
@@ -12,6 +12,7 @@ import { userRoutes } from "./user.routes";
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(userRoutes)
   ],
   declarations: [
