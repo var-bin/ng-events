bab91e8ea4d31b818ed1996eb440632f7049f4a5
var-bin <var-bin@users.noreply.github.com>
2018-08-23 23:13:55 +0300

    Implemented Model-driven Forms logic

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,7 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { UserAuthService } from "../user-auth.service";
 
 @Component({
   templateUrl: "./profile.component.html"
 })
 
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit {
+  profileForm: FormGroup;
+
+  constructor(
+    private userAuthService: UserAuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const firstName = new FormControl(this.userAuthService.firstName);
+    const lastName = new FormControl(this.userAuthService.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName,
+      lastName
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate["/events"];
+  }
+
+  onSubmit(data): void {
+    this.userAuthService.updateCurrentUser(
+      data.firstName,
+      data.lastName
+    );
+
+    this.router.navigate["/events"];
+  }
+}
