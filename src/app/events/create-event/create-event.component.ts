bf24170cc220e2695af52901a171895bac9c184d
var-bin <var-bin@users.noreply.github.com>
2018-08-24 20:39:50 +0300

    Added `saveEvent` method. Added a bunch of styles

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -2,15 +2,37 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
-  templateUrl: "./create-event.component.html"
+  templateUrl: "./create-event.component.html",
+  styles: [`
+    em { display: none; }
+
+    .error em {
+      display: block;
+      float: right;
+      color: red;
+    }
+
+    .error input {
+      background-color: antiquewhite;
+    }
+
+    .error input::placeholder {
+      color: crimson;
+    }
+  `]
 })
 
 export class CreateEventComponent {
   isDirty = false;
+  newEvent = {};
 
   constructor(private router: Router) {}
 
   cancel(): void {
     this.router.navigate(["/events"]);
+  }
+
+  saveEvent(formData) {
+    console.log(formData);
   }
 }
