d7b72e3a2dd5e05819bbe33c144f8d1a658a7a62
var-bin <var-bin@users.noreply.github.com>
2018-08-24 11:42:12 +0300

    Refactoring: Implementing checking logic for form controls via Model-driven Forms aka Reactive Forms

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 
 import { UserAuthService } from "../user-auth.service";
 
 @Component({
-  templateUrl: "./profile.component.html"
+  templateUrl: "./profile.component.html",
+  styles: [`
+    em { display: none; }
+
+    .profile-error em {
+      display: block;
+      float: right;
+      color: red;
+    }
+
+    .profile-error input {
+      background-color: antiquewhite;
+    }
+
+    .profile-error input::placeholder {
+      color: crimson;
+    }
+  `]
 })
 
 export class UserProfileComponent implements OnInit {
   profileForm: FormGroup;
+
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(
     private userAuthService: UserAuthService,
@@ -17,17 +37,23 @@ export class UserProfileComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const firstName = new FormControl(this.userAuthService.firstName);
-    const lastName = new FormControl(this.userAuthService.lastName);
+    this.firstName = new FormControl(
+      this.userAuthService.firstName,
+      Validators.required
+    );
+    this.lastName = new FormControl(
+      this.userAuthService.lastName,
+      Validators.required
+    );
 
     this.profileForm = new FormGroup({
-      firstName,
-      lastName
+      firstName: this.firstName,
+      lastName: this.lastName
     });
   }
 
   cancel(): void {
-    this.router.navigate["/events"];
+    this.router.navigate["events"];
   }
 
   onSubmit(data): void {
@@ -36,6 +62,14 @@ export class UserProfileComponent implements OnInit {
       data.lastName
     );
 
-    this.router.navigate["/events"];
+    this.router.navigate["events"];
+  }
+
+  getErrorCSSClass(controlName: string): string {
+    const ERROR_CSS_CLASS = "profile-error";
+
+    if (this[controlName].invalid && this[controlName].touched) {
+      return ERROR_CSS_CLASS;
+    }
   }
 }
