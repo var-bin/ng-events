32709cdc0eb219e81cf476c4abceb8d6f649ac79
var-bin <var-bin@users.noreply.github.com>
2018-08-27 16:06:37 +0300

    Added to barel `CreateSessionComponent`

--- src/app/events/index.ts
+++ src/app/events/index.ts
@@ -4,6 +4,7 @@ export { EventsListComponent } from "./events-list.component";
 export { EventThumbnailComponent } from "./event-thumnail.component";
 export { EventDetailsComponent } from "./event-details/event-details.component";
 export { CreateEventComponent } from "./create-event/create-event.component";
+export { CreateSessionComponent } from "./event-details/create-session/create-session.component"
 
 export { EventService } from "./shared/event.service";
 export { EventRouteActivator } from "./event-route-activator.service";
