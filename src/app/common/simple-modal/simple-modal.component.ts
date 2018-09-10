17469ba81a944c00c5853ecd3dbcf7f1f1d0a86c
var-bin <var-bin@users.noreply.github.com>
2018-09-10 16:36:04 +0300

    Added a new component for showing modal window

---
+++ src/app/common/simple-modal/simple-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "simple-modal",
+  templateUrl: "./simple-modal.component.html",
+  styles: [`
+    .modal-body {
+      height: 250px;
+      overflow-y: scroll;
+    }
+  `]
+})
+
+export class SimpleModalComponent {
+  @Input() title: string;
+}
