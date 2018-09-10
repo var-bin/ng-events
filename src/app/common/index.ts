a81fc807512880d406547314377931658283680b
var-bin <var-bin@users.noreply.github.com>
2018-09-10 16:34:41 +0300

    Create a barrel for all common components, services, directives, etc

---
+++ src/app/common/index.ts
@@ -0,0 +1,3 @@
+export { ModalTriggerDirective } from "./modal-trigger/modal-trigger.directive";
+export { SimpleModalComponent } from "./simple-modal/simple-modal.component";
+export { ToastrService } from "./toastr.service";
