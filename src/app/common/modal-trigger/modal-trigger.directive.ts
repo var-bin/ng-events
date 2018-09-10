e7db9fbc7a3c8cd6d8c09487421e7568a473bdde
var-bin <var-bin@users.noreply.github.com>
2018-09-10 17:28:17 +0300

    Implement logic for directive

--- src/app/common/modal-trigger/modal-trigger.directive.ts
+++ src/app/common/modal-trigger/modal-trigger.directive.ts
@@ -1,7 +1,23 @@
-import { Directive } from "@angular/core";
+import { Directive, OnInit, ElementRef } from "@angular/core";
+
+declare const $: any;
 
 @Directive({
   selector: "[modal-trigger]"
 })
 
-export class ModalTriggerDirective { }
+export class ModalTriggerDirective implements OnInit {
+  private el: HTMLElement;
+
+  constructor(
+    ref: ElementRef
+  ) {
+    this.el = ref.nativeElement;
+  }
+
+  ngOnInit() {
+    this.el.addEventListener("click", () => {
+      $("#simple-modal").modal();
+    });
+  }
+}
