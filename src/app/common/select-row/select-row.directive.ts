43f11eb9221dc2e6c0ba5becb6d94098a7724b18
var-bin <var-bin@users.noreply.github.com>
2018-09-11 12:28:00 +0300

    Implemented logic for `SelectedRowDirective`

--- src/app/common/select-row/select-row.directive.ts
+++ src/app/common/select-row/select-row.directive.ts
@@ -4,8 +4,78 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
   selector: "[select-row]"
 })
 
-export class SelectRowDirective {
-  el: HTMLElement;
+export class SelectRowDirective implements OnInit {
+  private el: HTMLElement;
 
-  constructor(private ref: ElementRef) {}
+  constructor(private ref: ElementRef) {
+    this.el = this.ref.nativeElement;
+  }
+
+  ngOnInit() {
+    this.el.addEventListener("click", (e) => {
+      this.onHighlightTr(e);
+    });
+
+    this.el.addEventListener("keydown", (e) => {
+      this.onArrowKeysHighlightTr(e);
+    })
+  }
+
+  onHighlightTr(event: Event) {
+    const TARGET = "TD";
+    const CLICKED_TARGET = <Element>event.target;
+    const CSS_CLASS = "is-selected";
+
+    if (CLICKED_TARGET.tagName === TARGET) {
+      this.removeHighlight(CSS_CLASS);
+      CLICKED_TARGET.parentElement.classList.add(CSS_CLASS);
+
+      console.log("Need add css class", CLICKED_TARGET, event);
+    }
+  }
+
+  onArrowKeysHighlightTr(event: KeyboardEvent) {
+    const CSS_CLASS = "is-selected";
+    const selected = this.el.querySelector(`.${CSS_CLASS}`);
+    const SIBLING_TARGET = "TR";
+    const doHighlightNextTr = selected && selected.nextElementSibling && selected.nextElementSibling.tagName === SIBLING_TARGET;
+    const doHighlightPrevTr = selected && selected.previousElementSibling && selected.previousElementSibling.tagName === SIBLING_TARGET;
+    const keyCode = event.keyCode;
+    const ENABLED_KEYS = {
+      arrowDown: 40,
+      arrowUp: 38
+    };
+    const isArrowDownPressed = ENABLED_KEYS.arrowDown === keyCode;
+    const isArrowUpPressed = ENABLED_KEYS.arrowUp === keyCode;
+
+    // there is no selected tr
+    if (!selected) {
+      return;
+    }
+
+    // move down
+    if (doHighlightNextTr && isArrowDownPressed) {
+      this.removeHighlight(CSS_CLASS);
+      selected.nextElementSibling.classList.add(CSS_CLASS);
+    }
+
+    // move up
+    if (doHighlightPrevTr && isArrowUpPressed) {
+      this.removeHighlight(CSS_CLASS);
+      selected.previousElementSibling.classList.add(CSS_CLASS);
+    }
+  }
+
+  private removeHighlight(cssClass: string) {
+    const _forEach = Array.prototype.forEach;
+    const selectedTrs = this.el.querySelectorAll(`.${cssClass}`);
+
+    console.log("selectedTrs: ", selectedTrs);
+
+    if (selectedTrs.length > 0) {
+      _forEach.call(selectedTrs, (tr: HTMLElement) => {
+        tr.classList.remove(cssClass);
+      });
+    }
+  }
 }
