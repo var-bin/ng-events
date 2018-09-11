4df9d9e6dad1ccc8ddf7083fe41f494f93ff7518
var-bin <var-bin@users.noreply.github.com>
2018-09-11 13:25:47 +0300

    Refactoring: Replace some variables to class private readonly props

--- src/app/common/select-row/select-row.directive.ts
+++ src/app/common/select-row/select-row.directive.ts
@@ -6,6 +6,13 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 
 export class SelectRowDirective implements OnInit {
   private el: HTMLElement;
+  private readonly ENABLED_KEYS = {
+    arrowDown: 40,
+    arrowUp: 38
+  };
+  private readonly CSS_CLASS = "is-selected";
+  private readonly TR_TAG = "TR";
+  private readonly TD_TAG = "TD";
 
   constructor(private ref: ElementRef) {
     this.el = this.ref.nativeElement;
@@ -22,31 +29,23 @@ export class SelectRowDirective implements OnInit {
   }
 
   onHighlightTr(event: Event) {
-    const TARGET = "TD";
     const CLICKED_TARGET = <Element>event.target;
-    const CSS_CLASS = "is-selected";
 
-    if (CLICKED_TARGET.tagName === TARGET) {
-      this.removeHighlight(CSS_CLASS);
-      CLICKED_TARGET.parentElement.classList.add(CSS_CLASS);
+    if (CLICKED_TARGET.tagName === this.TD_TAG) {
+      this.removeHighlight();
+      CLICKED_TARGET.parentElement.classList.add(this.CSS_CLASS);
 
       console.log("Need add css class", CLICKED_TARGET, event);
     }
   }
 
   onArrowKeysHighlightTr(event: KeyboardEvent) {
-    const CSS_CLASS = "is-selected";
-    const selected = this.el.querySelector(`.${CSS_CLASS}`);
-    const SIBLING_TARGET = "TR";
-    const doHighlightNextTr = selected && selected.nextElementSibling && selected.nextElementSibling.tagName === SIBLING_TARGET;
-    const doHighlightPrevTr = selected && selected.previousElementSibling && selected.previousElementSibling.tagName === SIBLING_TARGET;
+    const selected = this.el.querySelector(`.${this.CSS_CLASS}`);
+    const doHighlightNextTr = selected && selected.nextElementSibling && selected.nextElementSibling.tagName === this.TR_TAG;
+    const doHighlightPrevTr = selected && selected.previousElementSibling && selected.previousElementSibling.tagName === this.TR_TAG;
     const keyCode = event.keyCode;
-    const ENABLED_KEYS = {
-      arrowDown: 40,
-      arrowUp: 38
-    };
-    const isArrowDownPressed = ENABLED_KEYS.arrowDown === keyCode;
-    const isArrowUpPressed = ENABLED_KEYS.arrowUp === keyCode;
+    const isArrowDownPressed = this.ENABLED_KEYS.arrowDown === keyCode;
+    const isArrowUpPressed = this.ENABLED_KEYS.arrowUp === keyCode;
 
     // there is no selected tr
     if (!selected) {
@@ -55,26 +54,26 @@ export class SelectRowDirective implements OnInit {
 
     // move down
     if (doHighlightNextTr && isArrowDownPressed) {
-      this.removeHighlight(CSS_CLASS);
-      selected.nextElementSibling.classList.add(CSS_CLASS);
+      this.removeHighlight();
+      selected.nextElementSibling.classList.add(this.CSS_CLASS);
     }
 
     // move up
     if (doHighlightPrevTr && isArrowUpPressed) {
-      this.removeHighlight(CSS_CLASS);
-      selected.previousElementSibling.classList.add(CSS_CLASS);
+      this.removeHighlight();
+      selected.previousElementSibling.classList.add(this.CSS_CLASS);
     }
   }
 
-  private removeHighlight(cssClass: string) {
+  private removeHighlight() {
     const _forEach = Array.prototype.forEach;
-    const selectedTrs = this.el.querySelectorAll(`.${cssClass}`);
+    const selectedTrs = this.el.querySelectorAll(`.${this.CSS_CLASS}`);
 
     console.log("selectedTrs: ", selectedTrs);
 
     if (selectedTrs.length > 0) {
       _forEach.call(selectedTrs, (tr: HTMLElement) => {
-        tr.classList.remove(cssClass);
+        tr.classList.remove(this.CSS_CLASS);
       });
     }
   }
