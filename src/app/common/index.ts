ce629602e6f4cf6f51cd3a10870c99bf72ec22ff
var-bin <var-bin@users.noreply.github.com>
2018-09-11 12:26:09 +0300

    Added `SelectRowDirective` to barrel

--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -1,3 +1,4 @@
 export { ModalTriggerDirective } from "./modal-trigger/modal-trigger.directive";
 export { SimpleModalComponent } from "./simple-modal/simple-modal.component";
 export { ToastrService } from "./toastr.service";
+export { SelectRowDirective } from "./select-row/select-row.directive";
