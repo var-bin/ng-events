abedd7cc6b436085398663dc77ed8d9060660ce4
var-bin <var-bin@users.noreply.github.com>
2018-09-11 17:03:52 +0300

    Fix routing issue

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Params } from "@angular/router";
 
 import { EventService } from "../shared/event.service";
 
@@ -24,9 +24,10 @@ export class EventDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(
-      +this.route.snapshot.params["id"]
-    );
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params["id"]);
+      this.addMode = false;
+    });
   }
 
   toggleCreateForm(): void {
