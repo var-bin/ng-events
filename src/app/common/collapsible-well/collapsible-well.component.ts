28fa4cdb9f970bb4a182d38d58fe2d0e4165b66f
var-bin <var-bin@users.noreply.github.com>
2018-09-13 22:26:37 +0300

    Added a new component `collapsible-well` for testing <ng-content>

---
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "collapsible-well",
+  templateUrl: "./collapsible-well.component.html"
+})
+
+export class CollapsibleWellComponent {
+  @Input() title: string;
+  visible: boolean = true;
+
+  toggleContent(): void {
+    this.visible = !this.visible;
+  }
+}
