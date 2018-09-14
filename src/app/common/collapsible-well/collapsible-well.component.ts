154769794d006442a81da2a7e50fd5287b4b3029
var-bin <var-bin@users.noreply.github.com>
2018-09-14 21:11:25 +0300

    Refactoring: Using multiple slot content projection

--- src/app/common/collapsible-well/collapsible-well.component.ts
+++ src/app/common/collapsible-well/collapsible-well.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "collapsible-well",
@@ -6,7 +6,6 @@ import { Component, Input } from "@angular/core";
 })
 
 export class CollapsibleWellComponent {
-  @Input() title: string;
   visible: boolean = true;
 
   toggleContent(): void {
