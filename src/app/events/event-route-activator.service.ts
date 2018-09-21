b28f0e6cf5bb3448b10f3cce5fb62db8e62b2f35
var-bin <var-bin@users.noreply.github.com>
2018-09-21 21:45:35 +0300

    Refactoring: Use `InjectionToken` for creating service instead of `Injectable` service

--- src/app/events/event-route-activator.service.ts
+++ src/app/events/event-route-activator.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Inject } from "@angular/core";
 import { Router, CanActivate, ActivatedRouteSnapshot, CanDeactivate } from "@angular/router";
 
 import { CreateEventComponent } from "./create-event/create-event.component";
 
 import { EventService } from "./shared/event.service";
-import { ToastrService } from "../common/toastr.service";
+import { TOASTR_TOKEN, IToastr } from "../common/toastr.service";
 
 @Injectable()
 export class EventRouteActivator implements CanActivate, CanDeactivate<any> {
   constructor(
     private router: Router,
     private eventService: EventService,
-    private toastrService: ToastrService
+    @Inject(TOASTR_TOKEN) private toastrService: IToastr
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot) {
